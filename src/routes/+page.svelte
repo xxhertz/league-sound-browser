95e6cd13c89ea7b50b53e1ca8cd8b568776c3216
hz <47288761+xxhertz@users.noreply.github.com>
2024-09-21 16:59:45 -0400

    pretty unfunctional sidebar search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,18 @@
 	import EmoteList from "./components/EmoteList.svelte"
 	// import Search from "./components/Search.svelte"
 	export let data: { api: EmoteData[] }
+	let search: string
 </script>
 
 <div class="flex h-screen">
+	<aside class="float-left w-[33.33%] text-center py-32 px-16 bg-[radial-gradient(120%_100%_at_-20%_50%,rgba(24,24,27,1)_0%,rgba(9,9,11,255)_100%)]">
+		<input
+			class="outline-none ring-0 w-full h-12 font-semibold p-4 placeholder-current bg-zinc-950/40 border-2 border-transparent focus:border-eggshell text-eggshell"
+			placeholder="Search emotes..."
+			bind:value={search}
+		/>
+	</aside>
+
 	<EmoteList {data} />
 </div>
 
--- src/routes/components/EmoteSound.svelte
+++ src/routes/components/EmoteSound.svelte
@@ -5,8 +5,8 @@
 	let audio: HTMLAudioElement
 </script>
 
-<button on:click={() => audio.play()} class="group-hover:bg-opacity-75 bg-opacity-0 inset-0 rounded-2xl bg-zinc-950 w-full h-full absolute top-0 left-0 transition-colors duration-200">
-	<svg class="relative left-1/2 -translate-x-1/2 group-hover:opacity-75 opacity-0 transition-opacity duration-200" width="64" height="64" viewBox="0 0 64 64">
+<button on:click={() => audio.play()} class="group-hover:bg-opacity-75 bg-opacity-0 inset-0 rounded-2xl bg-zinc-950 w-full h-full absolute top-0 left-0 transition-colors duration-100">
+	<svg class="relative left-1/2 -translate-x-1/2 group-hover:opacity-75 opacity-0 transition-opacity duration-100" width="64" height="64" viewBox="0 0 64 64">
 		<circle cx="32" cy="32" r="31" class="stroke-zinc-950 stroke-2 opacity-50" />
 		<path d="M24 16v32l24-16L24 16z" class="stroke-ultraviolet stroke-2 fill-periwinkle" />
 	</svg>
