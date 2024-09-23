af9752bd64f479b513681fb2def51b8f62d9a34b
hz <47288761+xxhertz@users.noreply.github.com>
2024-09-23 03:09:55 -0400

    make grid responsive

--- src/components/EmoteList.svelte
+++ src/components/EmoteList.svelte
@@ -6,7 +6,7 @@
 	// forget it! let's make our own virtualizer! yay!
 </script>
 
-<main class="bg-zinc-950 w-full overflow-y-scroll grid-flow-row grid-cols-4 grid text-center">
+<main class="bg-zinc-950 w-full overflow-y-scroll grid-flow-row 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 p-4 gap-x-8 gap-y-6 grid text-center">
 	{#each data as emote}
 		<Emote {emote} />
 	{/each}
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -3,7 +3,7 @@
 	import Volume from "./sidebar/Volume.svelte"
 </script>
 
-<aside class="float-left w-[33.33%] text-center py-32 px-16 bg-[radial-gradient(120%_100%_at_-20%_50%,rgba(24,24,27,1)_0%,rgba(9,9,11,1)_100%)]">
+<aside class="w-[32rem] text-center py-32 px-16 bg-[radial-gradient(120%_100%_at_-20%_50%,rgba(24,24,27,1)_0%,rgba(9,9,11,1)_100%)]">
 	<Search />
 	<Volume />
 </aside>
