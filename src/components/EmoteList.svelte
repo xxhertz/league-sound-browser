44937fb3c5d07e831c22a0eeccd9dba5d4d1e8c3
hz <47288761+xxhertz@users.noreply.github.com>
2024-09-23 05:18:18 -0400

    commit for version control

--- src/components/EmoteList.svelte
+++ src/components/EmoteList.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
 	import Emote from "./emotelist/Emote.svelte"
 	import type { EmoteData } from "../../emote_compiler/shared"
+	import { onMount } from "svelte"
 	export let data: EmoteData[]
+	// forget it! let's make our own virtualizer! yay! all the svelte virtualizers are GARBAGE for grids
+	let columnCount = 4 //window.innerWidth >= 1536 ? 4 : window.innerWidth >= 1280 ? 3 : window.innerWidth >= 1024 ? 2 : 1
+	let container: HTMLElement
+	let measureCard: HTMLElement
+	onMount(() => {
+		const frame = requestAnimationFrame(function onFrame() {
+			console.log(window.innerWidth)
+			if (window.innerWidth >= 1536)
+				columnCount = 4 // 2xl
+			else if (window.innerWidth >= 1280)
+				columnCount = 3 // xl
+			else if (window.innerWidth >= 1024)
+				columnCount = 2 // lg
+			else columnCount = 1 // default (mobile first)
+			requestAnimationFrame(onFrame)
+		})
 
-	// forget it! let's make our own virtualizer! yay!
+		const containerRect = container.getBoundingClientRect()
+		const elementRect = measureCard.getBoundingClientRect()
+
+		return () => cancelAnimationFrame(frame)
+	})
 </script>
 
 <main class="bg-zinc-950 w-full overflow-y-scroll grid-flow-row 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 p-4 gap-x-8 gap-y-6 grid text-center">
+	<Emote emote={data[0]} hidden={true} />
 	{#each data as emote}
 		<Emote {emote} />
 	{/each}
--- src/components/emotelist/Emote.svelte
+++ src/components/emotelist/Emote.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte"
 	import type { EmoteData } from "../../../emote_compiler/shared"
 	import EmoteSound from "./EmoteSound.svelte"
+	export let hidden: boolean = false
 	export let emote: EmoteData
 	let image: HTMLImageElement
 	let card: HTMLDivElement
@@ -12,12 +13,10 @@
 
 <!-- I HATE NOT BEING ABLE TO ANIMATE THIS BACKGROUND GRADIENT I HATE IT SO MUCH THIS LOOKED SO MUCH COOLER BUT I HAD TO MAKE IT LOOK SO FUCKING LAME BECAUSE YOU CAN'T ANIMATE IT WITHOUT JS -->
 <!-- or custom css properties which are disgusting -->
-<div
-	class="emotecard text-eggshell bg-gradient-to-b w-full rounded-2xl hover:scale-[102%] hover:via-periwinkle/50 via-eggshell/0 transition-all duration-100 from-eggshell/0 to-zinc-950"
-	bind:this={card}
->
+<!-- also i totally wont kill myself prettier isn't completely disgusting kappa -->
+<div bind:this={card} class="emotecard text-eggshell bg-gradient-to-b w-full rounded-2xl hover:scale-[102%] hover:via-periwinkle/50 via-eggshell/0 transition-all duration-100 from-eggshell/0 to-zinc-950 {hidden && 'hidden'}">
 	<div class="relative group from-zinc-900 h-[calc(100%-2px)] w-[calc(100%-2px)] left-[1px] top-[1px] via-zinc-900 via-75% to-zinc-950 pb-4 pt-4 rounded-2xl bg-gradient-to-b">
-		<img class="lazy block w-64 h-64 m-auto" draggable="false" src="/finalized/{emote.id}.webp" alt="" bind:this={image} />
+		<img bind:this={image} class="lazy block w-64 h-64 m-auto" draggable="false" src="/finalized/{emote.id}.webp" alt="" />
 
 		{#if emote.hasOgg}
 			<EmoteSound {emote} />
