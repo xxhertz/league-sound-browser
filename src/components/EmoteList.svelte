d93a624e83295ec0ca234122c631977645788336
hz <alex@fini.work>
2024-09-23 15:05:28 -0400

    Add virtualization (#2)
    * successful virtualization of emotelist.svelte

--- src/components/EmoteList.svelte
+++ src/components/EmoteList.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-	import Emote from "./emotelist/Emote.svelte"
+	import { onMount } from "svelte"
 	import type { EmoteData } from "../../emote_compiler/shared"
-	export let data: EmoteData[]
+	import Emote from "./emotelist/Emote.svelte"
 
-	// forget it! let's make our own virtualizer! yay!
+	let scrollPos: number = 720 // good enough to render first 3 lines, will look odd on 4k and 1440p monitors but fixes on scroll
+
+	export let data: EmoteData[]
+	let main: HTMLElement
+	onMount(() => {
+		const updateScroll = () => (scrollPos = main.scrollTop + main.clientHeight)
+		main.addEventListener("scroll", updateScroll)
+		return () => main.removeEventListener("scroll", updateScroll)
+	})
 </script>
 
-<main class="bg-zinc-950 w-full overflow-y-scroll grid-flow-row 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 p-4 gap-x-8 gap-y-6 grid text-center">
-	{#each data as emote}
-		<Emote {emote} />
+<main bind:this={main} class="bg-zinc-950 w-full overflow-y-scroll grid-flow-row 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 p-4 gap-x-8 gap-y-6 grid text-center">
+	{#each data as emote, index}
+		<Emote {emote} {index} {scrollPos} />
 	{/each}
 </main>
--- src/components/emotelist/Emote.svelte
+++ src/components/emotelist/Emote.svelte
@@ -1,28 +1,32 @@
 <script lang="ts">
-	import { onMount } from "svelte"
 	import type { EmoteData } from "../../../emote_compiler/shared"
 	import EmoteSound from "./EmoteSound.svelte"
 	export let emote: EmoteData
-	let image: HTMLImageElement
-	let card: HTMLDivElement
-	onMount(() => {
-		if (window.innerHeight + window.scrollY > card.offsetTop) if (image.dataset.src) image.src = image.dataset.src
-	})
+	export let index: number
+	export let scrollPos: number
+
+	// tbh this can remain divided by 4, realistically it should be responsive like the document but idgaf it's good enough
+	const emoteHeight = 320 + 24 // 320px height, 24px of padding
+	const topOfBottomEmoteRow = Math.floor(index / 4) * emoteHeight
+	const topOfTopEmoteRow = Math.floor((index - 6 * 4) / 4) * emoteHeight
+	$: topOfScroll = scrollPos - 6 * emoteHeight // 6 rows
 </script>
 
 <!-- I HATE NOT BEING ABLE TO ANIMATE THIS BACKGROUND GRADIENT I HATE IT SO MUCH THIS LOOKED SO MUCH COOLER BUT I HAD TO MAKE IT LOOK SO FUCKING LAME BECAUSE YOU CAN'T ANIMATE IT WITHOUT JS -->
 <!-- or custom css properties which are disgusting -->
-<div
-	class="emotecard text-eggshell bg-gradient-to-b w-full rounded-2xl hover:scale-[102%] hover:via-periwinkle/50 via-eggshell/0 transition-all duration-100 from-eggshell/0 to-zinc-950"
-	bind:this={card}
->
-	<div class="relative group from-zinc-900 h-[calc(100%-2px)] w-[calc(100%-2px)] left-[1px] top-[1px] via-zinc-900 via-75% to-zinc-950 pb-4 pt-4 rounded-2xl bg-gradient-to-b">
-		<img class="lazy block w-64 h-64 m-auto" draggable="false" src="/finalized/{emote.id}.webp" alt="" bind:this={image} />
+<!-- forget it! let's make our own makeshift virtualizer! yay! all the svelte virtualizers are GARBAGE for grids -->
+{#if scrollPos > topOfBottomEmoteRow && topOfScroll < topOfBottomEmoteRow}
+	<div class="text-eggshell bg-gradient-to-b h-80 w-full rounded-2xl hover:scale-[102%] hover:via-periwinkle/50 via-eggshell/0 transition-all duration-100 from-eggshell/0 to-zinc-950">
+		<div class="relative group from-zinc-900 h-[calc(100%-2px)] w-[calc(100%-2px)] left-[1px] top-[1px] via-zinc-900 via-75% to-zinc-950 py-4 rounded-2xl bg-gradient-to-b">
+			<img class="lazy block w-64 h-64 m-auto" draggable="false" src="/finalized/{emote.id}.webp" alt="" />
 
-		{#if emote.hasOgg}
-			<EmoteSound {emote} />
-		{/if}
+			{#if emote.hasOgg}
+				<EmoteSound {emote} />
+			{/if}
 
-		<p class="font-semibold">{emote.name}</p>
+			<p class="font-semibold">{emote.name}</p>
+		</div>
 	</div>
-</div>
+{:else}
+	<div class="h-80" />
+{/if}
